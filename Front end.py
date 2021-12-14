0a0e31f22aadf44ff7f5f831eeca985241eacc7e
MohamedAouragh <Mohamed.aouragh@student.rocva.nl>
2021-12-14 13:20:08 +0100

    gokspel

--- Front end.py
+++ Front end.py
@@ -1,4 +1,8 @@
-kilometers = float(input("Aantal Kilometer: "))
-conv_fac = 0.6213713
-miles = kilometers * conv_fac
-print('%0.2f kilometers is gelijk aan %0.2f miles' %(kilometers,miles))
+guess = input("Wat is mijn lievelings eten?")
+
+if guess == "sushi":
+    print("Dat klopt goed gedaan!")
+else:
+    print("Goed geprobeert volgende keer beter")
+
+print("Bedankt voor het spelen!")
